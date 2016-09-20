bffb26ce2f3beede97a51c784b8c73983b345ebe
jorritmeulenbeld <jorritmeulenbeld@icloud.com>
2016-09-20 16:05:59 +0200

    added firebase 3 custom typings

--- systemjs.config.js
+++ systemjs.config.js
@@ -26,6 +26,7 @@
       // other libraries
       'rxjs':                       'npm:rxjs',
       'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+      'firebase':                   'npm:firebase'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -38,6 +39,10 @@
       },
       'angular2-in-memory-web-api': {
         main: './index.js',
+        defaultExtension: 'js'
+      },
+      'firebase': {
+        main: './firebase.js',
         defaultExtension: 'js'
       }
     }
